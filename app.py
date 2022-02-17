8b8962450a35cfb75edb3b4e9ec0babbf5c00e7e
Paco Leija <pacoleija@MacBook-Air-de-Paco.local>
2022-02-16 21:27:24 -0600

    Proyecto final

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask , request, render_template
+from flask import Flask, request, render_template, url_for, redirect
 
+from database import tasks
 
 app = Flask(__name__)
 
 #Si no le dices a ROUTE que tipo de metodo HTTP, por defaul usara un GET
 @app.route("/")
-def index():
-    return render_template("index.html")
+def Index():
+    return render_template("index.html",todoList = tasks)
  
 
 
@@ -18,12 +19,12 @@ def index():
 #def Welcomegym():
  #   return"Hola EBC"
 
-#@app.route("/", methods=['POST'])
-#def AddNewTask():
-#  if request.method == 'POST':
-      
-
+@app.route("/newTask", methods=['POST'])
+def AddNewTask():
+  if request.method == 'POST':
+    newTask = {"id": len(tasks) + 1, "name": request.form['task_name']}
+    tasks.append(newTask)
+    return redirect(url_for('Index'))
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
-    
+  app.run(port=5000, debug=True)
---
+++ database.py
@@ -0,0 +1,7 @@
+tasks = [
+  {"id": 1, "name": "Lavar los platos."},
+  {"id": 2, "name": "Sacar a pasear al perro."},
+  {"id": 3, "name": "Comprar foco de cocina."},
+  {"id": 4, "name": "Barrer."},
+  {"id": 5, "name": "Trapear."}
+]
